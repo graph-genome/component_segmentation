033c5a702fc9827de90de3e3fdd533988cfa3c1b
Josiah Seaman <josiah@newline.us>
2019-11-22 13:50:31 +0100

    Added Python object declaration

--- matrixcomponent/segmentation.py
+++ matrixcomponent/segmentation.py
@@ -1,2 +1,17 @@
 
 
+#Tasks
+# Get example output for tests - Ted
+# Parser for ODGI Bin file format - Ted
+# Component Segmentation Detection - Josiah and Joerg
+#   Python memory object model
+# Output format
+
+
+# For each Path
+#     For each node
+#         Is there a gap?
+#             Is the gap range anywhere else in this individual?
+#                 Divider should be somewhere in here
+#                 Tolerable range?
+#                 Stack up others using the same Link
