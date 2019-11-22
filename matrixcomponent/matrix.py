c2013c7e27ea73fda7aea6be9ed644e3880df308
Josiah Seaman <josiah@newline.us>
2019-11-22 14:37:15 +0100

    Added Path.__contains__ and some ill informed segmentation code

--- matrixcomponent/matrix.py
+++ matrixcomponent/matrix.py
@@ -6,6 +6,7 @@ class Path:
     def __init__(self):
         self.bins = []  # Bin
         self.links = []  # LinkEntry
+        self.__bin_set = set()
 
     class Bin:
         def __init__(self, bin_id, coverage, inversion_rate, mean_pos):
@@ -13,27 +14,30 @@ class Path:
             self.coverage = coverage
             self.inversion_rate = inversion_rate
             self.mean_pos = mean_pos
+            self.sequence = ''
 
     class LinkEntry:
         def __init__(self, left, right):
             self.left = left
             self.right = right
 
+    def __contains__(self, item):  # used by " x in Path "
+        if not self.__bin_set:
+            self.__bin_set = {x.bin_id for x in self.bins}  # build and cache a set
+        return item in self.__bin_set
+
 
 class Component:
     """Block of co-linear variation within a Graph Matrix"""
     def __init__(self):
-        self.matrix = [[]]  # square grid
+        self.matrix = [[]]  # square grid of Bin
+        # bin_id and seq are global to column and could be reduced to save memory,
+        # careful construction can reuse Bin.sequence memory pointer
         self.upstream = []  # Links
         self.downstream = []  # Links
 
     def split(self, abs_index):
-        """Splits one Component into two"""
+        """Splits one Component into two
+        :param abs_index the first position of the new component"""
         pass
-
-
-def test_construction():
-    p = Path()
-    p.bins = [Path.Bin(1,.9, 0, 0.11), Path.Bin(2,.8, 0, 0.15),]
-    p.links = [Path.LinkEntry(1,2)]
 
--- matrixcomponent/segmentation.py
+++ matrixcomponent/segmentation.py
@@ -1,3 +1,5 @@
+from typing import List
+from matrixcomponent.matrix import Path
 
 
 #Tasks
@@ -8,10 +10,18 @@
 # Output format
 
 
-# For each Path
-#     For each node
-#         Is there a gap?
-#             Is the gap range anywhere else in this individual?
-#                 Divider should be somewhere in here
-#                 Tolerable range?
-#                 Stack up others using the same Link
+def segment_matrix(matrix: List[Path]):
+    dividers = set()  # list of indices of new components
+
+    for path in matrix:  # TODO: brilliant except for the fact that bin_id is already sorted...
+        for x, bin in enumerate(path.bins):  # For each node
+            previous = path.bins[x - 1].bin_id
+            if x and bin.bin_id > previous + 1:  # Is there a gap?
+                missing_range = list(range(previous+1, bin.bin_id))
+                # Is the gap range anywhere else in this individual?
+                if any([i in path for i in missing_range if i > 0]):
+                    dividers.add(bin.bin_id + 1)  # the first position of the new component
+                    # TODO: insert prevarications about exact position
+                    # Divider should be somewhere in here
+                    # Tolerable range?
+                    # Stack up others using the same Link
