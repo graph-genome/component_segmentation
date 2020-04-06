1c2cad298cd28f32d3f3a6afd7337e098a16cbd5
Artem Tarasov <lomereiter@gmail.com>
2020-04-06 16:17:14 +0200

    use numpy for find_dividers operations

--- matrixcomponent/tests.py
+++ matrixcomponent/tests.py
@@ -5,7 +5,8 @@ from matrixcomponent.matrix import Path
 from matrixcomponent.utils import (
     numpy_links,
     path_boundaries,
-    self_loops
+    self_loops,
+    path_dividers
 )
 
 def test_numpy_links():
@@ -25,3 +26,24 @@ def test_self_loops():
                       [1, 1], [1, 3], [3, 3], [3, 0]])
     loops = np.unique(links[self_loops(links)], axis=0)
     assert np.array_equal(loops, [[1, 1], [3, 3]])
+
+
+def test_path_dividers():
+    bins = [1, 3, 4, 8, 15]
+
+    # test the simple case separately
+    links = np.array([[3, 1], [4, 1]])
+    assert np.array_equal(path_dividers(links, bins), links)
+
+    # test the general case
+    links = np.array([
+        [3, 1],  # u > d => yes
+        [1, 3],  # no bins within [1 + 1, 3) range => no
+        [3, 5],  # bin 4 is within [3 + 1, 5) range => yes
+        [5, 9],  # bin 8 is within [5 + 1, 9) => yes
+        [5, 8],  # no bins within [5 + 1, 9) => yes
+        [3, 4],  # empty bin range [3 + 1, 4) => no
+        [4, 1],  # u > d => yes
+    ])
+    assert np.array_equal(path_dividers(links, bins),
+                          [[3, 1], [3, 5], [5, 9], [4, 1]])
--- matrixcomponent/utils.py
+++ matrixcomponent/utils.py
@@ -19,3 +19,25 @@ def path_boundaries(links: np.array) -> np.array:
 def self_loops(links: np.array) -> np.array:
     ''' boolean mask of links where upstream equals downstream '''
     return links[:, 0] == links[:, 1]
+
+
+def path_dividers(links: np.array, bin_ids: np.array) -> np.array:
+    '''
+    Returns:
+      subarray of links that are considered dividers
+    Args:
+      links(np.array) - array of path links
+      bin_ids(np.array) - sorted array of path bin ids
+    '''
+    mask = links[:, 0] > links[:, 1]  # simple case: upstream > downstream
+
+    # if downstream >= upstream, we consider a link to be a divider
+    # iff there exists a bin id within [upstream + 1, downstream) range
+    ranges = np.copy(links[~mask])
+    ranges[:, 0] += 1   # each row is [upstream + 1, downstream]
+    indices = np.searchsorted(bin_ids, ranges)
+
+    # maintain the original order just in case
+    mask[~mask] = indices[:, 1] > indices[:, 0]
+
+    return links[mask]
