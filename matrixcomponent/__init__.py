6f37bb2d7a3631a5bebeb625f41fe095761398ec
subwaystation <simon.heumos@qbic.uni-tuebingen.de>
2020-04-10 20:33:50 +0200

    version 12 bump

--- matrixcomponent/__init__.py
+++ matrixcomponent/__init__.py
@@ -1,5 +1,5 @@
-JSON_VERSION = 10  # This version added adjacent connectors in departures column
-ODGI_VERSION = 10
+JSON_VERSION = 12  # This version added adjacent connectors in departures column
+ODGI_VERSION = 12
 
 import logging
 
