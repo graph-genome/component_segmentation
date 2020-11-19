22f16d5669e956eada41e1c40ca0ecb7ea9823aa
Josiah Seaman <josiah@newline.us>
2020-11-19 16:33:39 -0600

    Removed testing dependencies from the production version.

--- setup.py
+++ setup.py
@@ -10,16 +10,16 @@ setup(
     include_package_data=True,
     scripts=['matrixcomponent/segmentation.py'],
     install_requires=[
-        'nose>=1.3.7',
-        'rdflib>=4.2.2',
+        # 'nose>=1.3.7',
+        # 'pytest>=5.4.1',
         'DNASkittleUtils>=1.0.13',
-        'numpy>=1.18.2',
-        'pytest>=5.4.1',
-        'sortedcontainers>=2.1.0',
-        'joblib>=0.14.1',
-        'numba>=0.48.0',
-        'psutil>=5.7.0',
-        'recordclass>=0.13.2'
+        'sortedcontainers>=2.1',
+        'joblib>=0.14',
+        'psutil>=5.7',
+        'recordclass>=0.13'
+        'numpy>=1.9.3',
+        'numba>=0.48',
+        # 'rdflib>=4.2.2',
     ],
     url='https://graphgenome.org',
     download_url='https://github.com/graph-genome/component_segmentation',
@@ -32,6 +32,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
     ],
 )
